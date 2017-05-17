9f0046db24cd9ee0e8dc6abea1102fc9d9af25f2
Sarah Huff Moore <sarahhuffmoore@Sarahs-MacBook-Pro-9.local>
2017-05-17 07:47:27 -0400

    completed get routes

--- unit_02/express/pizza-express/app.js
+++ unit_02/express/pizza-express/app.js
@@ -4,6 +4,21 @@
 var express = require('express');
 //save an express module as 'app'
 var app     = express();
+
+app.get('/', function(req, res) {
+  res.send('Welcome to Pizza Express!');
+});
+
+app.get('/topping/:type', function(req, res, next) {
+
+    res.send('topping is pepp');
+});
+
+app.get('/order/:amount/:size', function(req, res, next) {
+
+    res.send( // something );
+});
+
 // assigning 3000 as our port
 var port    = 3000;
 
